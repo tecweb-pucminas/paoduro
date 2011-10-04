815a06ef9f5ff28ccc86907ebf8021e0dba2b210
Clebson <clebsoncarmo@yahoo.com.br>
2011-10-03 23:36:14 -0300

    paoduro

---
+++ mapa.php
@@ -0,0 +1,73 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+    <title></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <style type="text/css" >
+        
+#map{
+   width: 100px;
+   height: 100px;
+   position: absolute;
+   top: 30%;
+   left: 33%;
+   margin-top: -50px;
+   margin-left: -50px;
+   
+}
+#map_center{
+    padding: 1px 0 50px 0;
+    
+}
+#posit a:link, #posit a:visited{
+    background: url("../assets/images/bord_menu.png") no-repeat scroll 0 0 transparent;
+    font-family: "lucida sans unicode";
+    font-size: 78%;
+    height: 24px;
+    padding: 1px 10px 4px 9px;
+    text-align: center;
+    text-transform: lowercase;
+    width: 153px;
+    border: 1px solid #98B692;
+    text-transform: lowercase;
+    text-decoration:  none;
+    color: black;
+}
+#posit img{
+    clear: both;
+    margin-bottom: 15px;
+}
+#posit{
+    width: 15%;
+}
+
+#posit h4 {
+    background: url("../assets/images/bord_cima.png") no-repeat scroll 0 0 transparent;
+    font-family: "lucida sans unicode";
+    font-size: 79%;
+    height: 19px;
+    padding: 5px;
+    width: 226px;
+}
+    </style>
+  </head>
+  <body>
+      
+      <div  id="map">
+          <img id="map_center" src="../assets/images/mapa_centro.jpg">
+      </div>
+      <div id="posit">
+          <h4>Comerciantes mais próximos</h4>
+          <img src="../assets/images/mapa_peq.jpg">
+         
+              <a href="#">Comerciantes Próximos</a>
+          
+
+      </div>
+    
+  </body>
+</html>
